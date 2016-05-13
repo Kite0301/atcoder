f154e002f931870967b32ec2fcf15d129873daf6
Kite0301 <kaito.titech@gmail.com>
2016-05-13 09:42:43 +0900

    mujin/b

---
+++ mujin/b.rb
@@ -0,0 +1,21 @@
+# a=gets.split.map &:to_i
+# l = a.inject(:+)
+# s = [a.max*2-l, 0].max
+# c = Math::PI
+# p (l*l-s*s)*c
+
+
+
+a=gets.split.map &:to_i
+l=a.inject(:+)
+p (l*l-[a.max*2-l,0].max**2)*Math::PI
+
+
+# a=gets.split.map &:to_i;l=a.inject(:+);p (l*l-[a.max*2-l,0].max**2)*Math::PI
+
+
+# ---------------------------------------------------------
+# http://mujin-pc-2016.contest.atcoder.jp/submissions/647925
+
+a,b,c=gets.split.map &:to_f
+p 12.56637*((s=a+b+c)-t=[a,b,c,s/2].max)*t
