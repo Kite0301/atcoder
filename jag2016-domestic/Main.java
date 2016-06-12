aaa006ed97c880c0923df7c60021c2fe90a93fd3
Kite0301 <kaito.titech@gmail.com>
2016-06-12 23:26:25 +0900

    jag2016-domestic/Main.java

--- jag2016-domestic/Main.java
+++ jag2016-domestic/Main.java
@@ -40,3 +40,21 @@ class a {
 }
 
 // import java.util.*;class a{public static void main(String[] args){Scanner sc=new Scanner(System.in);int f=0;String x="YES";int n=sc.nextInt();for(int i=0;i<n;i++){String t=sc.next();if(t.equals("A")){f++;}else{f--;if(f<0){x="NO";}}}if(f!=0){x="NO";}System.out.println(x);}}
+
+class b{
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int x=0;
+    int s=0;
+    int n = sc.nextInt();
+    int m = sc.nextInt();
+    int t = sc.nextInt();
+    for (int i=0;i<n;i++){
+      int a = sc.nextInt();
+      x += Math.max(a-m-s, 0);
+      s = a;
+    }
+    x += Math.max(t-m-s, 0);
+    System.out.println(x);
+  }
+}
