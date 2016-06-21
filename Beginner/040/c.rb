3a6a82b5cb8ab44fbbc613ffcd5d64d9473f4eee
Kite0301 <kaito.titech@gmail.com>
2016-06-21 21:15:19 +0900

    Beginner/040/c.rb

--- Beginner/040/c.rb
+++ Beginner/040/c.rb
@@ -1,14 +1,28 @@
-n=gets.to_i
-a=gets.split.map &:to_i
-c=[]
-s=a.pop
-f=a.pop
-c<<0
-c<<(s-f).abs
-(n-2).times{
-  m=a.pop
+# n=gets.to_i-2
+# a=gets.split.map &:to_i
+# c=[]
+# s=a.pop
+# f=a.pop
+# c<<0
+# c<<(s-f).abs
+# n.times{
+#   m=a.pop
+#   c<<[(s-m).abs+c[-2],(f-m).abs+c[-1]].min
+#   s=f
+#   f=m
+# }
+# p c[-1]
+
+
+
+# -------------------------------------------
+
+
+n=gets.to_i-2
+a=gets.split.reverse.map &:to_i
+c=[0,(a[0]-a[1]).abs]
+n.times{|i|
+  s,f,m=a[i..2+i]
   c<<[(s-m).abs+c[-2],(f-m).abs+c[-1]].min
-  s=f
-  f=m
 }
 p c[-1]
